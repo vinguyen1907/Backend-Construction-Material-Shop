a458dffb179e6857513a614ce4288c500d4a8379
Nguyễn Tiến Vĩ <94362297+vinguyen1907@users.noreply.github.com>
2023-12-11 09:36:18 +0700

    fix: fix cors

--- src/main/java/com/example/cmsbe/config/CorsConfig.java
+++ src/main/java/com/example/cmsbe/config/CorsConfig.java
@@ -1,32 +1,9 @@
 package com.example.cmsbe.config;
 
-//import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.web.cors.CorsConfiguration;
-//import org.springframework.web.cors.CorsConfigurationSource;
-//import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import java.util.Arrays;
-import java.util.List;
-
-//@Configuration
-//@EnableWebSecurity
-//public class CorsConfig {
-//    @Bean
-//    public CorsConfigurationSource corsConfigurationSource() {
-//        CorsConfiguration configuration = new CorsConfiguration();
-//        configuration.setAllowedOrigins(List.of("http://localhost:8000"));
-//        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
-//        configuration.setAllowedHeaders(Arrays.asList("Content-Type", "Authorization"));
-//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-//        source.registerCorsConfiguration("/api/v1/**", configuration);
-//        return source;
-//    }
-//}
 
 @Configuration
 @EnableWebMvc
