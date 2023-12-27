2b0e8009f72e2208d2cb0ff704057e4d06e98c51
Nguyễn Tiến Vĩ <94362297+vinguyen1907@users.noreply.github.com>
2023-12-27 13:41:01 +0700

    chore: use pagination dto

--- src/main/java/com/example/cmsbe/controllers/OrderController.java
+++ src/main/java/com/example/cmsbe/controllers/OrderController.java
@@ -1,16 +1,15 @@
 package com.example.cmsbe.controllers;
 
+import com.example.cmsbe.dto.PaginationDTO;
 import com.example.cmsbe.models.Order;
 import com.example.cmsbe.models.enums.OrderStatus;
 import com.example.cmsbe.services.OrderService;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/orders")
@@ -19,7 +18,7 @@ public class OrderController {
     private final OrderService orderService;
 
     @GetMapping
-    public ResponseEntity<List<Order>> getAllOrders(
+    public ResponseEntity<PaginationDTO<Order>> getAllOrders(
             @RequestParam(defaultValue = "0") int page,
             @RequestParam(defaultValue = "10") int size,
             @RequestParam(required = false) Integer id,
@@ -28,19 +27,12 @@ public class OrderController {
             @RequestParam(required = false) LocalDate startDate,
             @RequestParam(required = false) LocalDate endDate
     ) {
-        if (id != null || customerName != null || status != null || startDate != null || endDate != null) {
-            return ResponseEntity.ok(orderService.searchWithFilter(page, size, id, customerName, status, startDate, endDate));
-        }
-        return  ResponseEntity.ok(orderService.getAllOrders(page, size));
+        return ResponseEntity.ok(orderService.searchWithFilter(page, size, id, customerName, status, startDate, endDate));
     }
 
     @GetMapping("/{orderId}")
-    public ResponseEntity<?> getOrderById(@PathVariable Integer orderId) {
-        try {
-            return ResponseEntity.ok(orderService.getOrderById(orderId));
-        } catch (EntityNotFoundException e) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
-        }
+    public ResponseEntity<?> getOrderById(@PathVariable Integer orderId) throws EntityNotFoundException {
+        return ResponseEntity.ok(orderService.getOrderById(orderId));
     }
 
     @PostMapping
@@ -49,15 +41,11 @@ public class OrderController {
     }
 
     @PutMapping("/{orderId}")
-    public ResponseEntity<?> updateOrder(@PathVariable Integer orderId, @RequestParam(name = "status") OrderStatus newStatus) {
-        try {
-            var result = orderService.updateOrder(orderId, newStatus);
-            return ResponseEntity.ok(result);
-        } catch (EntityNotFoundException e) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
-        } catch (Exception e) {
-            return ResponseEntity.internalServerError().build();
-        }
+    public ResponseEntity<?> updateOrder(
+            @PathVariable Integer orderId,
+            @RequestParam(name = "status") OrderStatus newStatus
+    ) throws EntityNotFoundException {
+        return ResponseEntity.ok(orderService.updateOrder(orderId, newStatus));
     }
 
     @DeleteMapping("/{orderId}")
--- src/main/java/com/example/cmsbe/services/interfaces/IOrderService.java
+++ src/main/java/com/example/cmsbe/services/interfaces/IOrderService.java
@@ -1,5 +1,6 @@
 package com.example.cmsbe.services.interfaces;
 
+import com.example.cmsbe.dto.PaginationDTO;
 import com.example.cmsbe.models.Order;
 import com.example.cmsbe.models.enums.OrderStatus;
 
@@ -7,13 +8,13 @@ import java.time.LocalDate;
 import java.util.List;
 
 public interface  IOrderService {
-    List<Order> getAllOrders(int page, int size);
+    PaginationDTO<Order> getAllOrders(int page, int size);
     Order getOrderById(Integer id);
     Order createOrder(Order order);
     Order updateOrder(Integer orderId, OrderStatus newStatus);
     void deleteOrder(Integer orderId);
     // customer id, created time, status
-    List<Order> searchByIdAndStatusAndCreatedTime(int page, int size, Integer id, OrderStatus status, LocalDate startDate, LocalDate endDate);
-    List<Order> searchByCustomerNameAndStatusAndCreatedTime(int page, int size, String customerName, OrderStatus status, LocalDate startDate, LocalDate endDate);
-    List<Order> searchWithFilter(int page, int size, Integer id, String customerName, OrderStatus status, LocalDate startDate, LocalDate endDate);
+    PaginationDTO<Order> searchByIdAndStatusAndCreatedTime(int page, int size, Integer id, OrderStatus status, LocalDate startDate, LocalDate endDate);
+    PaginationDTO<Order> searchByCustomerNameAndStatusAndCreatedTime(int page, int size, String customerName, OrderStatus status, LocalDate startDate, LocalDate endDate);
+    PaginationDTO<Order> searchWithFilter(int page, int size, Integer id, String customerName, OrderStatus status, LocalDate startDate, LocalDate endDate);
 }
